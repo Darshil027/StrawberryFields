2318704fb84a8342645be981e914dfcae2f6ca3f
Abhinavjoshi7 <92068770+Abhinavjoshi7@users.noreply.github.com>
2023-02-17 20:39:32 -0700

    Added index page

---
+++ index.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Strawberry Fields</title>
+  </head>
+  <body>
+
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Strawberry Fields</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="register.php">Register</a>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Welcome to Strawberry Fields</h1>
+        <p class="lead">We are a family-run, pesticide-free Strawberry U-Pick farm located in Southwest Edmonton.</p>
+      </div>
+    </div>
+
+    <!-- About Section -->
+<section id="about" class="py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h2>About Strawberry Fields</h2>
+        <p>Strawberry Fields is a family-run, pesticide-free Strawberry U-Pick farm located in Southwest Edmonton. We believe that everyone deserves access to sweet, juicy, and delicious strawberries no matter where they are. Whether you’re snacking on them as a treat or using them to make your favorite dessert, you can always count on our strawberries to be the best. Come and taste the difference at Strawberry Fields!</p>
+        <h3>Mission</h3>
+        <p>Our mission at Strawberry Fields is to provide fresh, delicious, and healthy strawberries to customers everywhere. We strive to deliver the highest quality fruit through sustainable farming practices and innovative technology.</p>
+        <h3>Vision</h3>
+        <p>Our vision is to be the preferred source for fresh, premium strawberries globally. We aim to revolutionize the way people enjoy and access the sweet, juicy, and healthy fruit by fostering a community of dedicated farmers, innovative growers, and loyal customers.</p>
+        <h3>Values</h3>
+        <p>Our values are the foundation of our business, and we are committed to upholding them as we continue to bring the taste of nature's sweetness to the world. We believe in Quality, Sustainability, and Health.</p>
+      </div>
+      <div class="col-md-6">
+        <h3>Why Choose Us</h3>
+        <ul class="list-group">
+          <li class="list-group-item"><i class="fas fa-check"></i> Choose us for the freshest, highest-quality strawberries, sustainably grown and picked at the peak of ripeness for unparalleled taste and nutrition.</li>
+          <li class="list-group-item"><i class="fas fa-check"></i> Healthier option for consumption.</li>
+          <li class="list-group-item"><i class="fas fa-check"></i> Grown without the use of harmful chemicals.</li>
+          <li class="list-group-item"><i class="fas fa-check"></i> Our strawberries tend to have a more natural and intense flavor.</li>
+          <li class="list-group-item"><i class="fas fa-check"></i> A unique and enjoyable experience with care.</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- End of About Section -->
+<footer class="bg-light py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h5>Contact Info</h5>
+        <p>Strawberry Fields, 140 Grandisle Rd NW, Edmonton, AB T6M 2P1</p>
+        <p>strawberryfieldsedm@gmail.com</p>
+      </div>
+      <div class="col-md-4">
+        <h5>Service</h5>
+        <ul class="list-unstyled">
+          <li><a href="#">Picking Appointment</a></li>
+        </ul>
+      </div>
+      <div class="col-md-4">
+        <h5>Get In Touch</h5>
+        <ul class="list-unstyled">
+          <li><a href="#">Follow us through our Social Media for regular updates.</a></li>
+        </ul>
+      </div>
+    </div>
+    <hr>
+    <p class="small text-muted">© 2023 Strawberry Fields. All rights reserved. Powered by Strawberry Fields.</p>
+  </div>
+</footer>
+</body>
